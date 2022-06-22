e5c168a68c8936ad7bb55c129e14e4daa1c095d8
Humita <98102129+HumitaFN@users.noreply.github.com>
2022-06-23 00:01:52 +0900

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+//prevent right click
+document.addEventListener("contextmenu", event => event.preventDefault());
+
+//bg canvas
+VANTA.WAVES({
+    el: ".bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3a3ab3,
+    waveSpeed: 0.50,
+    zoom: 0.65
+})
+
+//links
+document.getElementById("github").addEventListener("click", () => window.location.href = "https://github.com/HumitaFN")
+document.getElementById("youtube").addEventListener("click", () => window.location.href = "https://www.youtube.com/channel/UCM8BajvghRokd36mESQQ-FQ")
+document.getElementById("discord").addEventListener("click", () => window.location.href = "https://www.youtube.com/channel/UCM8BajvghRokd36mESQQ-FQ")
+document.getElementById("twitch").addEventListener("click", () => window.location.href = "https://www.twitch.tv/fnhumita")
+document.getElementById("twitter").addEventListener("click", () => window.location.href = "https://twitter.com/Hum1ta")
+
+//img automation
+document.querySelectorAll("img").forEach(e => {
+    e.src = `assets/logos/${e.id}.png`
+    e.alt = e.id
+    e.addEventListener("mouseover", () => e.style.filter = "invert(0)")
+    e.addEventListener("mouseout", () => e.style.filter = "invert(1)")
+})
